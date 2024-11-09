0dd937a840ff7250c2d8edea59169415886b5748
Tuti Wulandari <adidata.tuti@bankmandiri.co.id>
2024-11-09 20:27:54 +0700

    remove the unused import

--- src/test/java/id/dbs/testing/AutomateLoginTest.java
+++ src/test/java/id/dbs/testing/AutomateLoginTest.java
@@ -1,6 +1,5 @@
 package id.dbs.testing;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
